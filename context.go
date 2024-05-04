610c611dbec078175d145e6e9840bfc0e145dbc4
justjack1521 <henryt1521@hotmail.com>
2024-05-04 22:04:37 +1000

    null exception when getting key from metadata in context

--- context.go
+++ context.go
@@ -9,22 +9,17 @@ import (
 
 var ErrUnableToParseMetaData = errors.New("unable to parse metadata")
 var ErrMetaDataContainsMalformedUserID = errors.New("metadata contains malformed user uuid")
+var ErrMetaDataContainsMalformedPlayerID = errors.New("metadata contains malformed player uuid")
 
 func UserIDFromContext(ctx context.Context) uuid.UUID {
 	md, ok := metadata.FromIncomingContext(ctx)
 	if ok == false {
 		return uuid.Nil
 	}
-	val := md.Get("X-API-USER")[0]
-	return uuid.FromStringOrNil(val)
-}
-
-func PlayerIDFromContext(ctx context.Context) uuid.UUID {
-	md, ok := metadata.FromIncomingContext(ctx)
-	if ok == false {
+	if len(md.Get("X-API-USER")) == 0 {
 		return uuid.Nil
 	}
-	val := md.Get("X-API-PLAYER")[0]
+	val := md.Get("X-API-USER")[0]
 	return uuid.FromStringOrNil(val)
 }
 
@@ -32,6 +27,9 @@ func MustUserIDFromContext(ctx context.Context) (uuid.UUID, error) {
 	md, ok := metadata.FromIncomingContext(ctx)
 	if ok == false {
 		return uuid.Nil, ErrUnableToParseMetaData
+	}
+	if len(md.Get("X-API-USER")) == 0 {
+		return uuid.Nil, ErrMetaDataContainsMalformedUserID
 	}
 	client := md.Get("X-API-USER")[0]
 	user, err := uuid.FromString(client)
@@ -41,15 +39,30 @@ func MustUserIDFromContext(ctx context.Context) (uuid.UUID, error) {
 	return user, nil
 }
 
+func PlayerIDFromContext(ctx context.Context) uuid.UUID {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if ok == false {
+		return uuid.Nil
+	}
+	if len(md.Get("X-API-PLAYER")) == 0 {
+		return uuid.Nil
+	}
+	val := md.Get("X-API-PLAYER")[0]
+	return uuid.FromStringOrNil(val)
+}
+
 func MustPlayerIDFromContext(ctx context.Context) (uuid.UUID, error) {
 	md, ok := metadata.FromIncomingContext(ctx)
 	if ok == false {
 		return uuid.Nil, ErrUnableToParseMetaData
 	}
+	if len(md.Get("X-API-PLAYER")) == 0 {
+		return uuid.Nil, ErrMetaDataContainsMalformedPlayerID
+	}
 	client := md.Get("X-API-PLAYER")[0]
 	player, err := uuid.FromString(client)
 	if uuid.Equal(player, uuid.Nil) || err != nil {
-		return uuid.Nil, ErrMetaDataContainsMalformedUserID
+		return uuid.Nil, ErrMetaDataContainsMalformedPlayerID
 	}
 	return player, nil
 }
