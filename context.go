9e0b80333e34d84a2b11ce30939fc33909839665
justjack1521 <henryt1521@hotmail.com>
2024-05-06 20:30:27 +1000

    updated error handling

--- context.go
+++ context.go
@@ -12,9 +12,13 @@ const (
 	PlayerIDMetadataKey = "X-API-PLAYER"
 )
 
-var ErrUnableToParseMetaData = errors.New("unable to parse metadata")
-var ErrMetaDataContainsMalformedUserID = errors.New("metadata contains malformed user uuid")
-var ErrMetaDataContainsMalformedPlayerID = errors.New("metadata contains malformed player uuid")
+var (
+	errUnableToParseMetaData             = errors.New("unable to parse metadata")
+	errUserIDMissingFromMetaData         = errors.New("user id missing from metadata")
+	errMetaDataContainsMalformedUserID   = errors.New("metadata contains malformed user uuid")
+	errMetaDataContainsMalformedPlayerID = errors.New("metadata contains malformed player uuid")
+	errPlayerIDMissingFromMetaData       = errors.New("player id missing from metadata")
+)
 
 func UserIDFromContext(ctx context.Context) uuid.UUID {
 	md, ok := metadata.FromIncomingContext(ctx)
@@ -31,15 +35,15 @@ func UserIDFromContext(ctx context.Context) uuid.UUID {
 func MustUserIDFromContext(ctx context.Context) (uuid.UUID, error) {
 	md, ok := metadata.FromIncomingContext(ctx)
 	if ok == false {
-		return uuid.Nil, ErrUnableToParseMetaData
+		return uuid.Nil, errUnableToParseMetaData
 	}
 	if len(md.Get(UserIDCMetadataKey)) == 0 {
-		return uuid.Nil, ErrMetaDataContainsMalformedUserID
+		return uuid.Nil, errUserIDMissingFromMetaData
 	}
 	client := md.Get(UserIDCMetadataKey)[0]
 	user, err := uuid.FromString(client)
 	if uuid.Equal(user, uuid.Nil) || err != nil {
-		return uuid.Nil, ErrMetaDataContainsMalformedUserID
+		return uuid.Nil, errMetaDataContainsMalformedUserID
 	}
 	return user, nil
 }
@@ -59,15 +63,15 @@ func PlayerIDFromContext(ctx context.Context) uuid.UUID {
 func MustPlayerIDFromContext(ctx context.Context) (uuid.UUID, error) {
 	md, ok := metadata.FromIncomingContext(ctx)
 	if ok == false {
-		return uuid.Nil, ErrUnableToParseMetaData
+		return uuid.Nil, errUnableToParseMetaData
 	}
 	if len(md.Get(PlayerIDMetadataKey)) == 0 {
-		return uuid.Nil, ErrMetaDataContainsMalformedPlayerID
+		return uuid.Nil, errPlayerIDMissingFromMetaData
 	}
 	client := md.Get(PlayerIDMetadataKey)[0]
 	player, err := uuid.FromString(client)
 	if uuid.Equal(player, uuid.Nil) || err != nil {
-		return uuid.Nil, ErrMetaDataContainsMalformedPlayerID
+		return uuid.Nil, errMetaDataContainsMalformedPlayerID
 	}
 	return player, nil
 }
